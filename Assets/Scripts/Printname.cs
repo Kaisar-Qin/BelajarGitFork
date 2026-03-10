782cd1506d4eeb5313b960920c15920c1e0ac8f4
Kaisar-Qin <kaisardeno@gmail.com>
2026-03-10 10:37:00 +0700

    Menambahkan nama

---
+++ Assets/Scripts/Printname.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Printname : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Nama saya Kaisar deno ankabut");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
